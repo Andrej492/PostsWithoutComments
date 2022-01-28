08b32545ca82ae17835ac1e60703e5ee4799ac63
Andrej492 <andrejmatovic1@gmail.com>
2022-01-28 15:46:47 +0100

    Added Amplify REST Api, DynamoDb, Cognito, Lambda

---
+++ PostsWithoutComments/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,38 @@
+export type AmplifyDependentResourcesAttributes = {
+    "storage": {
+        "postsTable": {
+            "Name": "string",
+            "Arn": "string",
+            "StreamArn": "string",
+            "PartitionKeyName": "string",
+            "PartitionKeyType": "string",
+            "Region": "string"
+        }
+    },
+    "function": {
+        "postsLambda": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        }
+    },
+    "auth": {
+        "postswithoutcomments": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "api": {
+        "postsRestApi": {
+            "RootUrl": "string",
+            "ApiName": "string",
+            "ApiId": "string"
+        }
+    }
+}
