f231add5e7dd8dc31dcaa00e7d3ff3166e200200
Andrej492 <andrejmatovic1@gmail.com>
2022-01-28 16:03:47 +0100

    Added neccessary components

--- PostsWithoutComments/src/app/app.module.ts
+++ PostsWithoutComments/src/app/app.module.ts
@@ -4,10 +4,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AmplifyUIAngularModule } from '@aws-amplify/ui-angular';
 import { AppComponent } from './app.component';
+import { AuthComponent } from './auth/auth.component';
+import { HeaderComponent } from './header/header.component';
+import { PostsComponent } from './posts/posts.component';
+import { PostListComponent } from './posts/post-list/post-list.component';
+import { PostDetailComponent } from './posts/post-detail/post-detail.component';
+import { PostEditComponent } from './posts/post-edit/post-edit.component';
+import { PostItemComponent } from './posts/post-list/post-item/post-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthComponent,
+    HeaderComponent,
+    PostsComponent,
+    PostListComponent,
+    PostDetailComponent,
+    PostEditComponent,
+    PostItemComponent
   ],
   imports: [
     BrowserModule,
