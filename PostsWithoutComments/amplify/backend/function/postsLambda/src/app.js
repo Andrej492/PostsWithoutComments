fdc3579a4c63d6b486faf8ef806acd04fdd9667f
Andrej492 <andrejmatovic1@gmail.com>
2022-01-28 17:40:01 +0100

    Adding get and get/:id to backend Lambda

--- PostsWithoutComments/amplify/backend/function/postsLambda/src/app.js
+++ PostsWithoutComments/amplify/backend/function/postsLambda/src/app.js
@@ -1,13 +1,3 @@
-/*
-Copyright 2017 - 2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.
-Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with the License. A copy of the License is located at
-    http://aws.amazon.com/apache2.0/
-or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and limitations under the License.
-*/
-
-
-
 const AWS = require('aws-sdk')
 var awsServerlessExpressMiddleware = require('aws-serverless-express/middleware')
 var bodyParser = require('body-parser')
@@ -38,9 +28,9 @@ app.use(bodyParser.json())
 app.use(awsServerlessExpressMiddleware.eventContext())
 
 // Enable CORS for all methods
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*")
-  res.header("Access-Control-Allow-Headers", "*")
+app.use(function (request, response, next) {
+  response.header("Access-Control-Allow-Origin", "*")
+  response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
   next()
 });
 
@@ -58,34 +48,23 @@ const convertUrlType = (param, type) => {
  * HTTP Get method for list objects *
  ********************************/
 
-app.get(path + hashKeyPath, function(req, res) {
-  var condition = {}
-  condition[partitionKeyName] = {
-    ComparisonOperator: 'EQ'
-  }
-
-  if (userIdPresent && req.apiGateway) {
-    condition[partitionKeyName]['AttributeValueList'] = [req.apiGateway.event.requestContext.identity.cognitoIdentityId || UNAUTH ];
-  } else {
-    try {
-      condition[partitionKeyName]['AttributeValueList'] = [ convertUrlType(req.params[partitionKeyName], partitionKeyType) ];
-    } catch(err) {
-      res.statusCode = 500;
-      res.json({error: 'Wrong column type ' + err});
-    }
-  }
-
-  let queryParams = {
+app.get("/posts", function(request, response) {
+  let params = {
     TableName: tableName,
-    KeyConditions: condition
+    limit: 100,
   }
-
-  dynamodb.query(queryParams, (err, data) => {
+  dynamodb.scan(params, (err, result) => {
     if (err) {
-      res.statusCode = 500;
-      res.json({error: 'Could not load items: ' + err});
+      response.json({
+        statusCode: 500,
+        error: 'Could not load items: ' + err
+      });
     } else {
-      res.json(data.Items);
+      response.json({
+        statusCode: 200,
+        url: request.url,
+        body: JSON.stringify(result.Items)
+      });
     }
   });
 });
@@ -94,42 +73,28 @@ app.get(path + hashKeyPath, function(req, res) {
  * HTTP Get method for get single object *
  *****************************************/
 
-app.get(path + '/object' + hashKeyPath + sortKeyPath, function(req, res) {
-  var params = {};
-  if (userIdPresent && req.apiGateway) {
-    params[partitionKeyName] = req.apiGateway.event.requestContext.identity.cognitoIdentityId || UNAUTH;
-  } else {
-    params[partitionKeyName] = req.params[partitionKeyName];
-    try {
-      params[partitionKeyName] = convertUrlType(req.params[partitionKeyName], partitionKeyType);
-    } catch(err) {
-      res.statusCode = 500;
-      res.json({error: 'Wrong column type ' + err});
-    }
-  }
-  if (hasSortKey) {
-    try {
-      params[sortKeyName] = convertUrlType(req.params[sortKeyName], sortKeyType);
-    } catch(err) {
-      res.statusCode = 500;
-      res.json({error: 'Wrong column type ' + err});
-    }
-  }
-
-  let getItemParams = {
+app.get("/posts/:id", function(request, response) {
+  let params = {
     TableName: tableName,
-    Key: params
+    Key: {
+      id: request.params.id
+    }
   }
-
-  dynamodb.get(getItemParams,(err, data) => {
+  dynamodb.get(params, (err, result) => {
     if(err) {
-      res.statusCode = 500;
-      res.json({error: 'Could not load items: ' + err.message});
+      response.json({
+        statusCode: 200,
+        error: 'Could not load items: ' + err.message
+      });
     } else {
-      if (data.Item) {
-        res.json(data.Item);
+      if (result.Item) {
+        response.json({
+          statusCode: 200,
+          url: request.url,
+          body: JSON.stringify(result.item)
+        });
       } else {
-        res.json(data) ;
+        response.json(result) ;
       }
     }
   });
