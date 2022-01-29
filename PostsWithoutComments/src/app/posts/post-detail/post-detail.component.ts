cf9fd223c7010a177af4965c1932701935ff2df8
Andrej492 <andrejmatovic1@gmail.com>
2022-01-29 18:35:18 +0100

    Removed dropdown and added buttons instead for edit, delete post and add comment

--- PostsWithoutComments/src/app/posts/post-detail/post-detail.component.ts
+++ PostsWithoutComments/src/app/posts/post-detail/post-detail.component.ts
@@ -1,15 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { Post } from '../post.model';
+import { PostService } from '../post.service';
 
 @Component({
   selector: 'app-post-detail',
   templateUrl: './post-detail.component.html',
   styleUrls: ['./post-detail.component.css']
 })
-export class PostDetailComponent implements OnInit {
+export class PostDetailComponent implements OnInit, OnDestroy {
+  post: Post;
+  id: number;
+  isLoggedIn = false;
+  subscriptionAuth: Subscription;
 
-  constructor() { }
+  constructor(
+    private postService: PostService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    this.route.params.subscribe((params: Params) => {
+      this.id = +params['id'];
+      this.post = this.postService.getPost(this.id);
+    });
+
+    this.subscriptionAuth = this.postService.isAuthenticated.subscribe((isAuth: boolean) => {
+      this.isLoggedIn = isAuth;
+    });
+  }
+
+  onEditPost() {
+    this.router.navigate(['edit'], {relativeTo: this.route});
+    //this.router.navigate(['../', this.id, 'edit], {relativeTo: this.route});
+  }
+
+  onDeletePost() {
+    this.postService.deletePost(this.id);
+    this.router.navigate(['/posts']);
+  }
+
+  onAddComment() {}
+
+  ngOnDestroy(): void {
+    this.subscriptionAuth.unsubscribe();
   }
 
 }
--- PostsWithoutComments/src/app/posts/post-edit/post-edit.component.ts
+++ PostsWithoutComments/src/app/posts/post-edit/post-edit.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { Post } from '../post.model';
+import { PostService } from '../post.service';
 
 @Component({
   selector: 'app-post-edit',
@@ -6,10 +10,62 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./post-edit.component.css']
 })
 export class PostEditComponent implements OnInit {
+  id: number;
+  editMode = false;
+  postForm: FormGroup;
 
-  constructor() { }
+  constructor(
+    private postService: PostService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    this.route.params.subscribe((params: Params) => {
+      this.id = +params['id'];
+      this.editMode = params['id'] != null;
+      this.initForm();
+    });
+  }
+
+  onSubmit() {
+    if (this.editMode) {
+      this.postService.updatePost(this.id, this.postForm.value);
+    } else {
+      this.postService.addPost(this.postForm.value);
+    }
+    this.onCancel();
+  }
+
+  onCancel() {
+    this.router.navigate(['../'], { relativeTo: this.route});
+  }
+
+  onClear() {
+    this.postForm.reset();
+    this.editMode = false;
+  }
+
+  ngOnDestroy(): void {
+      //this.editPostSubscription.unsubscribe();
+  }
+
+  private initForm() {
+    let Title = '';
+    let ImagePath = '';
+    let Content = '';
+
+    if (this.editMode) {
+      const post: Post = this.postService.getPost(this.id);
+      Title = post.postTitle;
+      ImagePath = post.postImagePath;
+      Content = post.postContent;
+    }
+    this.postForm = new FormGroup({
+      'postTitle': new FormControl(Title, Validators.required),
+      'postImagePath': new FormControl(ImagePath, Validators.required),
+      'postContent': new FormControl(Content)
+    });
   }
 
 }
